411291a9a434d6b7bace14a3a21f9069f111c62b
Caleb Holt <Caleb.Holt@octanner.com>
2016-11-16 14:17:48 -0700

    real first commit

---
+++ src/main/java/com/octanner/AlademoApplication.java
@@ -0,0 +1,12 @@
+package com.octanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlademoApplication.class, args);
+	}
+}
